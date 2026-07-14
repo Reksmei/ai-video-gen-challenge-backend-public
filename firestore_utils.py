4c75af1fd7c8b6d72881584b1ae1601c6de364f3
Reksmei Arkadiusz-Davidavic <reksmeiad@gmail.com>
2026-07-14 16:43:43 +0100

    Update copyright information in firestore_utils.py
    Updated copyright year and author name in the file header.

--- firestore_utils.py
+++ firestore_utils.py
@@ -1,16 +1,16 @@
-Copyright [Year] [Your Name]
+# Copyright 2026 Reksmei Arkadiusz-Davidavic
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-    http://apache.org
+#    http://apache.org
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from firebase_admin import firestore, credentials
 import firebase_admin
